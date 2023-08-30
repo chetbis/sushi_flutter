2986a8d3b05b6d4832a98daec879a8c2a50d3cf0
chetbis <chetiabishnu95@gmail.com>
2023-08-30 23:31:26 +0530

    add intro page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sushi/components/button.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 138, 60, 55),
+        body: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'SUSHI MAN',
+                  style: GoogleFonts.dmSerifDisplay(
+                    fontSize: 28,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 25),
+                Padding(
+                  padding: const EdgeInsets.all(50.0),
+                  child: Image.asset('lib/images/fish_eggs.png'),
+                ),
+                const SizedBox(height: 25),
+                Text(
+                  'THE TASTE OF JAPANESE FOOD',
+                  style: GoogleFonts.dmSerifDisplay(
+                    fontSize: 44,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Feel the taste of the most popular Japanese food from anywhere and anytime.',
+                  style: TextStyle(
+                    height: 2,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(height: 25),
+                MyButton(
+                  text: 'Get Started',
+                  onTap: () {
+                    // go to the menu page
+                  },
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
